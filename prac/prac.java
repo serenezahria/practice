971f6e46ca851aea163ebe68a9f40094eaa16087
serenezahria <145510376+serenezahria@users.noreply.github.com>
2025-02-26 23:51:49 -0500

    Update prac.java

--- prac/prac.java
+++ prac/prac.java
@@ -3,9 +3,9 @@ public class prac {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		int a = 6;
-		int b = 6;
-		int c = 6;
+		int a = 7;
+		int b = 7;
+		int c = 7;
 		if (b*b - 4*a*c <0)
 			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
 		else
