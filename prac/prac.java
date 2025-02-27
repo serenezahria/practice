791e9ecd781ab83a09ee079b8d65973f11269720
serenezahria <serenea@umich.edu>
2025-02-26 23:42:12 -0500

    initial

---
+++ prac/prac.java
@@ -0,0 +1,18 @@
+import java.lang.*;
+public class prac { 
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 6;
+		int b = 6;
+		int c = 6;
+		if (b*b - 4*a*c <0)
+			System.out.println("This equation " + a + "x^2 + " + b +"x + " + c +" has no solution");
+		else
+			{
+				System.out.println("The solution for the equation " + a + "x^2 + " + b +"x + " + c +" are");
+				System.out.println((-b+Math.sqrt(b*b-4*a*c))/2*a + " and " + (-b-Math.sqrt(b*b-4*a*c))/2*a);
+			}
+	}
+
+}
